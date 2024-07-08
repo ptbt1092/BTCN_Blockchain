079eb9e59bc42c12d3bfe659b457cabbba984b25
20120392 <ptbt1092@gmail.com>
2024-07-08 14:58:23 +0700

    Setup Handlebars view engine

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
 const indexRouter = require('./routes/index');
 const app = express();
 const port = 3000;
@@ -8,6 +9,10 @@ const port = 3000;
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.engine('hbs', exphbs({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+
 app.use('/', indexRouter);
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello from the router!');
+    res.render('home', { message: 'Hello from Handlebars!' });
 });
 
 module.exports = router;
