41636f03c5e24efc2a762866478cdc06c5d5be45
20120392 <ptbt1092@gmail.com>
2024-07-08 14:57:06 +0700

    Setup basic routing structure

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const indexRouter = require('./routes/index');
 const app = express();
 const port = 3000;
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use('/', indexRouter);
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
