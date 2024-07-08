b4c6ee0c30b4f7c36f4b8a56c8c2d0930b22b546
20120392 <ptbt1092@gmail.com>
2024-07-08 14:56:05 +0700

    Add morgan and body-parser middleware

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
